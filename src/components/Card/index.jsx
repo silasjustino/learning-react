2b8527ee8e5ff93fd9a058c94992057cf7865f64
silasjustino <silasjustino99@gmail.com>
2022-08-04 19:57:25 -0300

    components

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export function Card() {
+    return(
+        <div className='card'>
+            <strong>Silas Justino</strong>
+            <small>10:15:23</small>
+        </div>
+    )
+}
