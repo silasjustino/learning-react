b75e675ffb1278e1d328a703801d4cced13b683b
silasjustino <silasjustino99@gmail.com>
2022-08-04 23:30:00 -0300

    changed useEffect to async function

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -23,14 +23,16 @@ export function Home() {
   }
 
   useEffect(() => {
-    fetch('https://api.github.com/users/silasjustino')
-    .then(response => response.json())
-    .then(data => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/silasjustino')
+      const data = await response.json()
       setUser({
         name: data.name,
         avatar: data.avatar_url,
       })
-    })
+    }
+
+    fetchData()
   }, [])
 
   return (
